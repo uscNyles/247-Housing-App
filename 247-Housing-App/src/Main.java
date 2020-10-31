4a6107b0a7a7e8b79114f897f64a529943ceb610
uscNyles <70451912+uscNyles@users.noreply.github.com>
2020-10-31 14:40:05 -0400

    Slight cleanup

--- 247-Housing-App/src/Main.java
+++ 247-Housing-App/src/Main.java
@@ -34,76 +34,9 @@ public class Main {
 			else
 				setCurrentUser(ui.createUser(userTypeChoice));
 		}
-
-		if (currentUser instanceof Seller)
-			UserAPI.createSeller((Seller) currentUser);
-		if (currentUser instanceof RealEstateAgent)
-			UserAPI.createRE((RealEstateAgent) currentUser);
-		if (currentUser instanceof Renter && (((Renter) currentUser).getSeller() != null)) {
-			UserAPI.createRenter((Renter) currentUser);
-			UserAPI.createSeller((Seller) currentUser);
-		} else if (currentUser instanceof Renter) {
-			UserAPI.createRenter((Renter) currentUser);
-		}
-
 	}
 
 	public static void addProperty(Seller sellerUser) {
-//		System.out.println("Name:");
-//		String name = s.nextLine();
-//		System.out.println("Street Address:");
-//		String address = s.nextLine();
-//		System.out.println("Zip Code:");
-//		String zipCode = s.nextLine();
-//		System.out.println("City:");
-//		String city = s.nextLine();
-//		System.out.println("State:");
-//		String state = s.nextLine();
-//		System.out.println("Description:");
-//		String description = s.nextLine();
-//		System.out.println("Condition:");
-//		String condition = s.nextLine();
-//		System.out.println("Number of rooms:");
-//		int roomNumber = s.nextInt();
-//		System.out.println("List of Amenities:");
-//		ArrayList<String> amenities = new ArrayList<String>();
-//		boolean done = false;
-//		while (done == false) { // loop until user is done entering contents of amenities list
-//			System.out
-//					.println("Enter \"done\" when all you are finished adding" + " amenities to this property listing");
-//			String amenity = s.nextLine();
-//			if (amenity.equalsIgnoreCase("done")) {
-//				done = true;
-//			} else {
-//				amenities.add(amenity);
-//			}
-//		}
-//		System.out.println("Cost per Month:");
-//		double price = s.nextDouble();
-//		// System.out.println("Subleasing potential");
-//		// we may be forbidding leasing functionality
-//
-//		System.out.println("Type of property (apartment, condo, or house):");
-//		PropertyType propertyType = PropertyType.APARTMENT; // must initialize to avoid error
-//		done = false;
-//		while (done == false) { // loop to make sure user enters a valid property type
-//			String userPropType = s.nextLine();
-//			if (userPropType.equalsIgnoreCase("house")) {
-//				propertyType = PropertyType.HOUSE;
-//				done = true;
-//			} else if (userPropType.equalsIgnoreCase("apartment")) {
-//				propertyType = PropertyType.APARTMENT;
-//				done = true;
-//			} else if (userPropType.equalsIgnoreCase("condo")) {
-//				propertyType = PropertyType.CONDO;
-//				done = true;
-//			} else {
-//				System.out.println("please enter a valid Property type.");
-//			}
-//		}
-//
-//		PropertyAPI.createProperty(new Property(sellerUser, condition, condition, condition, condition, condition,
-//				condition, roomNumber, amenities, price, propertyType));
 		ui.outputMenu("addProperty");
 		ui.addProperty(sellerUser);
 	}
